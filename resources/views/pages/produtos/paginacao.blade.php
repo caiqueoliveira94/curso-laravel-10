eb7c5b49c71fc87fa9bcf70d29195171e0b06510
caiqueoliveira94 <oliveira.caique@outlook.com.br>
2024-07-27 10:23:34 -0300

    import jquery e clockUI

--- resources/views/pages/produtos/paginacao.blade.php
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -1,17 +1,20 @@
-@extends('index');
+@extends('index')
 
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Produtos</h1>
     </div>
     <div>
-        <form action="" method="get">
+        <form action="{{ route('produtos.index') }}" method="get">
             <input type="text" name="pesquisar" id="" placeholder="Digite o nome">
             <button class="btn btn-primary btn-sm">Pesquisar</button>
             <a type="button" href="" class="btn btn-success float-end">Incluir Produto</a>
         </form>
-        <h2>Section title</h2>
+        
         <div class="table-responsive mt-4">
+            @if ($findProduto->isEmpty())
+                <p>Não existe dados.</p>
+            @else
             <table class="table table-striped table-sm">
                 <thead>
                     <tr>
@@ -26,13 +29,14 @@
                             <td>{{ $produto->nome }}</td>
                             <td>{{ 'R$ ' . number_format($produto->valor, 2, ',', '.') }}</td>
                             <td>
-                                <a href="" class="btn btn-light btn-sm">Editar</a>
+                                <a href="{{ route('produtos.delete') }}" class="btn btn-light btn-sm">Editar</a>
                                 <a href="" class="btn btn-danger btn-sm">Excluir</a>
                             </td>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+            @endif
         </div>
     </div>
 @endsection
